58d573cf0f675c40bb00a034929996d2c3a86e7c
Qenawi <aldmanmero@yahoo.com>
2021-04-11 21:54:40 +0200

    init commit

---
+++ app/src/test/java/com/example/androidunittest_cicd/MainActivityUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.androidunittest_cicd
+
+import org.junit.Assert
+import org.junit.Test
+
+class MainActivityUnitTest {
+  @Test
+  fun testInit() {
+    Assert.assertEquals(4, 2 + 2)
+  }
+}
